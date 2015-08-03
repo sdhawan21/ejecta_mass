2cf9ef4563118fb2c57ed3cd254ca43266847692
sdhawan21 <suhail.dhawan@gmail.com>
2015-08-03 12:39:36 +0200

    with errors on t0

--- fid_time.py
+++ fid_time.py
@@ -4,6 +4,8 @@ Code to calculate Ejecta masses of SNIa
 .. - Use equation 24 and equation A16 of Jeffrey 1999
 .. - Use values for e-folding velocity, absorption opacity and q-factor (traces Ni distribution)
 
+
+Note: requires pack to be install on the machine 
 """
 
 import numpy as np
@@ -29,34 +31,85 @@ class fid_time:
     """
 	calculate the fiducial time for a given bolometric light curve (Ni mass is obtained using Arnett's rule)
     """
-    def __init__(self, mni):
+    def __init__(self, mni, e_mni):
+        """
+        Initiate the class with a Nickel mass measurement
+        """
         self.mni=mni
-    def edp(self, t, t0):
+        self.e_mni = e_mni
+    
+    def edp_mc(self, t, t0):    
     	"""
-    	Energy deposition function. eqn 2 Stritzinger 2006 (also see Nadyozhin 1994)
+    	Do Monte Carlo over the space of Mni values 
+    	
+    	Energy deposition function. eqn 2 Stritzinger 2006 (also see Nadyozhin 1994 for a more detailed description of the derivation)
     	
     	"""
-        Nni=self.numb(self.mni)
+    	
+    	#if errors need to be calculated by Monte Carlo
+    	
+	
+	Nni=self.numb(np.random.normal(self.mni, self.e_mni))
+	
+      
+        t1=lni*Nni*np.exp(-lni*t)*(1.75)		#ENi
+	
+	t2=lco*Nni*(lni/(lni-lco))*(np.exp(-lco*t)-np.exp(-lni*t))	#ECo_e+
+	t3=(0.12)+(3.61)*(1-np.exp(-(t0/t)**2))		#ECo_gamma
+
+	return (t1+t2*t3)/(fac*ds2)#(t2*t3)/(fac*ds2)
+	
+	
+    
+    def edp_nomc(self, t, t0):
+    	"""
+    	Energy deposition function. eqn 2 Stritzinger 2006 (also see Nadyozhin 1994 for a more detailed description of the derivation)
+    	
+    	"""
+    	#if errors need to be calculated by Monte Carlo
+    	
+	Nni = self.numb(self.mni)
+	
         t1=lni*Nni*np.exp(-lni*t)*(1.75)		#ENi
 	t2=lco*Nni*(lni/(lni-lco))*(np.exp(-lco*t)-np.exp(-lni*t))	#ECo_e+
 	t3=(0.12)+(3.61)*(1-np.exp(-(t0/t)**2))		#ECo_gamma
-	return (t1+t2*t3)/(fac*ds2)#(t2*t3)/(fac*ds2)
+	return (t1+t2*t3)/(fac*ds2)
+
+	
+	
     def numb(self, mni):
     	"""
     	Convert Ni mass to number of Ni atoms
     	"""
         return mni*1.98e33*6.022e23/56.0
+    
+    
+    
     def ejm(self, t0):
         """
         Using Equation 4 of scalzo et al. to convert t0 into ejecta mass
+        
         (talk to Wolfgang H. about this equation)
         """
+        
         return 8*np.pi*(((t0)*ds2)**2)*9e6*sc*3/(0.025*1.98e33)
-    def val_calc(self, sn):
+        
+        
+    def ejm_mc(self, t, n=10000):
+    
+    	real = np.random.normal
+    
+    	ar=[self.ejm(real(t[0], t[1])) for k in range(n)]
+    	return np.mean(ar), np.std(ar) 
+    
+    def val_calc(self, sn, p):
+    
 	"""
 	A standalone function to calculate the fiducial time and ejecta mass for a given bolometric light curve
 	"""
-    	filename = '/Users/lapguest/bol_ni_ej/lcbol_distrib/'+sn+'_lcbol_u_CSPB_CSPV_CSPr_CSPi_CSPJ_CSPH_CSP.dat'
+    
+    	filename = p+'bol_ni_ej/lcbol_distrib/'+sn+'_lcbol_u_CSPB_CSPV_CSPr_CSPi_CSPJ_CSPH_CSP.dat'
+    	
     	t=bol.bol_func().bolpeak(filename)
     	bollc=np.loadtxt(filename)
     	bollc[:,0]-=t[1]
@@ -78,59 +131,101 @@ class q_fac:
 		d=quad(self.func, ran[0], ran[1])
 		return self.q/d[0]
 
-    	
+   
 def main(path):
+
     filelist=np.loadtxt(sys.argv[1], dtype='string')
-<<<<<<< HEAD
+
     #ipath='/Users/lapguest/newbol/bol_ni_ej/'
     
     #define the file prefix and suffix
-=======
+
     #path='/Users/lapguest/newbol/bol_ni_ej/'
->>>>>>> 823d0c6cb5edf68063aca52f01288ba14fea8fb1
+
     pre='lcbol_distrib/'
     suf='_lcbol_u_CSPB_CSPV_CSPr_CSPi_CSPJ_CSPH_CSP.dat'
     arr=[]
     for i in filelist:
         try:
+            #complete filename 
             f=path+pre+i[0]+suf
     
-            bollc=np.loadtxt(f, usecols=(0, 1))
-            #mni=float(i[1])
+    	    #light curve 
+            bollc=np.loadtxt(f, usecols=(0, 1, 2))
+            #mni=float(i[1])\
+            
+            #define functions for the peak and error of the bolometric lc
+            
             bp=bol.bol_func().bolpeak
+            
+            err_func= bol.bol_func().err_peak
+            
             tmax=bp(f)[1]
-
+	    
+	   
             #derive Ni mass from the light curve (Arnett's rule)
-            mni=bp(f)[0]/2e43
-	
-	    dm15 = -(mni -1.3) /0.62
+            
+            mni=bp(f)[0]/2e43		#needs to be a function of the rise time
+	   
+	    e_mni = bp(f)[2]/2e43
+	    
+	    dm15 = - (mni -1.3) /0.62
+	    
             #check whether there is pre-maximum bolometric coverage
             assert tmax != bollc[0][0]
-            bollc[:,0]-=tmax
-            fid=fid_time(mni)
-            b=bollc[(bollc[:,0]>40.) & (bollc[:,0] < 100.)]
-            popt, pcov = curve_fit(fid.edp, b[:,0], b[:,1])
             
+            bollc[:,0]-=tmax
+            fid=fid_time(mni, e_mni)
+            
+            #define the phase range for the curve fit
+            b=bollc[(bollc[:,0]>40.) & (bollc[:,0] < 100.)]
+            print max(b[:,0])
+            print 'Curve fitting', i[0]
+            
+            #best curvefit parameters
+            param_arr=[]
+            
+            #for k in range(10000):
+            popt, pcov = curve_fit(fid.edp_mc, b[:,0], b[:,1], p0=[20.])
+            #    print popt
+          
+            perr = np.sqrt(np.diag(pcov))
+            
+#            	param_arr.append(popt[0])
+           
             #calculate rise time using ganeshalingham et al. 
             rt=16.5-5.*(dm15-1.1)
-            
-            arr.append([i[0], mni, popt[0], popt[0]+rt,fid.ejm(popt[0]+rt)])
+        
+        
+            t0=popt[0]+rt
+        
+            #error in ejecta mass from curve fit
 
+	    
+            ej, e_ej = fid.ejm_mc([t0, perr[0]])
+	                
+            print "For", i[0], "the error in the peak is", perr[0]
+            
+            #five column array of name,  nickel mass, t0(post max), t0, mej, err    
+            arr.append([i[0], mni, popt[0], t0, round(ej, 3), round(e_ej, 3)])
+	
             #plot the light curve and the fit to late data
-           # plt.plot(bollc[:,0], bollc[:,1], 'r.')
+            """
+            plt.plot(bollc[:,0], bollc[:,1], 'r.')
             #plt.plot(b[:,0], fid.edp(b[:,0], popt[0]))
             #plt.show()
+	    """
         except:
             i
     np.savetxt(sys.argv[2], arr, fmt='%s')
-<<<<<<< HEAD
-if __name__=="__main__":	
+
+if __name__=="__main__":
+	
 	if len(sys.argv) == 4:
+    		
     		main(sys.argv[3])
     	else:
     		print "Usage: python", sys.argv[0], '<input file> <outfilename> <path to lcs>'
-=======
-if __name__=="__main__":
-    main(sys.argv[3])
->>>>>>> 823d0c6cb5edf68063aca52f01288ba14fea8fb1
+		print "Example: python", sys.argv[0], '<fileinp_ejecmass.txt> <ejecmass> </home/sdhawan>'
+
 
