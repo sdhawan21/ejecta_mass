aa5aa7029d7b366abf89cebeb7cc663f5d0366a4
sdhawan21 <suhail.dhawan@gmail.com>
2015-12-11 10:18:16 +0100

    chisq by handwritten func

--- ej.py
+++ ej.py
@@ -4,6 +4,7 @@ To vet the code against what Max used for his analysis
 
 
 Everything presumes a 19d rise time
+
 """
 
 import numpy as np
@@ -16,11 +17,32 @@ from pack import fid_time,bol
 from scipy.interpolate import interp1d
 
 def peak(lc):
-
+    """
+    simple spline fitter for bolometric light curves
+    """
     l=np.linspace(lc[:,0].min(), lc[:,0].max(), 100)
     spl=interp1d(lc[:,0], lc[:,1], kind='cubic')
     gl = spl(l)
     return max(gl), l[gl==max(gl)][0]
+
+def chisq_by_hand(f, t0, obs, err=False):
+	"""
+	chi2 = (model - flux)**2/err**2
+	"""
+	
+        edep = f(obs[:,0], t0)
+	
+	flux = obs[:,1]
+	
+	if err == False:
+            chisq = np.sum((edep-flux)**2)
+        else:
+            chisq = np.sum((edep-flux)**2/obs[:,2]**2)
+	return chisq
+
+def fit_chisq(f, obs, tarr=np.linspace(0, 50, 100)):
+	chiarr = np.array([chisq_by_hand(f, i, obs) for i in tarr])
+	return tarr[chiarr==min(chiarr)][0], min(chiarr)
 
 #doesnt require a function form yet
 bp = bol.bol_func().bolpeak
@@ -35,10 +57,13 @@ if max(bollc[:,0]) < 500:
     tmax = 0
     mmax = max(bollc[:,1])
 
+#shift to maximum light
 bollc[:,0]-=tmax
 
+#define the late light curve as between +40 - +100 d 
 tail = bollc[(bollc[:,0] > 40) & (bollc[:,0] < 100)]
 
+#risetime from command line
 risetime= float(sys.argv[2])
 coef = bol.arn_coef(risetime)
 mni = mmax/(coef*1e43)
@@ -46,13 +71,32 @@ ft = fid_time.fid_time(mni)
 
 print "The inferred Nickel mass is:", mni
 
-popt, pcov = curve_fit(ft.edp_nomc, tail[:,0], tail[:,1], sigma=tail[:,2])
+popt, pcov = curve_fit(ft.edp_nomc, tail[:,0], tail[:,1], sigma=tail[:,2], p0=[20.])
 
 t0= popt[0]+risetime
 print "fiducial timescale, t0 is:", t0, pcov[0]
 print "Ejecta mass is ", ft.ejm(t0), ft.ejm_mc((t0, pcov[0]+3))
 
 t=np.linspace(0, 200, 500)
-plt.errorbar(bollc[:,0]+risetime, bollc[:,1],bollc[:,2], fmt='rs')
-plt.plot(t, ft.edp_nomc(t, t0))
+print "The t0 and chisq minimum"
+print fit_chisq(ft.edp_nomc, tail), chisq_by_hand(ft.edp_nomc, 28, tail)
+
+
+
+#plotting the results
+plt.figure(1)
+plt.errorbar(bollc[:,0], bollc[:,1],bollc[:,2], fmt='rs')
+plt.plot(t-risetime, ft.edp_nomc(t, t0), label='t0='+str(t0))
+plt.plot(t-risetime, ft.edp_nomc(t, 28), label='t0=28')
+
+plt.legend(loc=0)
+plt.xlabel('Phase (days)')
+plt.ylabel('Bolometric Flux')
+
+#chisq plot
+plotchi = False
+if plotchi == True:
+    plt.figure(2)
+    tarr=np.linspace(0, 50, 100)
+    plt.plot(tarr, np.array([chisq_by_hand(ft.edp_nomc,i, tail) for i in tarr]))
 plt.show()
